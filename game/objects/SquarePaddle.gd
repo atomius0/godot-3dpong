57752e9935e0e68333eec41cf6aa2cff0bcd8ad8
atomius <atomius1@gmail.com>
2017-11-05 12:09:10 +0100

    cleaned up the code

--- game/objects/SquarePaddle.gd
+++ game/objects/SquarePaddle.gd
@@ -5,8 +5,6 @@ onready var paddle_range = init_paddle_range()
 onready var input_handler = get_node("InputHandler")
 
 func _ready():
-	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	#set_process_input(true)
 	set_fixed_process(true)
 
 #TODO: use paddle size to determine range, otherwise paddle will partially go through the walls.
@@ -22,31 +20,14 @@ func init_paddle_range():
 	)
 	return paddle_range
 
+
 func limit_paddle_range(move_to):
 	var pr = paddle_range # shorthand
 	var move_to_v2 = Vector2(clamp(move_to.x, pr.pos.x, pr.end.x), clamp(move_to.y, pr.pos.y, pr.end.y))
 	return Vector3(move_to_v2.x, move_to_v2.y, move_to.z)
 
 
-#var movement = Vector2()
-
-#func _input(event):
-#	# TODO: what is the input event type for mouse motion?
-#	if (event.type == InputEvent.MOUSE_MOTION):
-#		movement = movement + event.relative_pos
-#	pass
-
-
 func _fixed_process(delta):
 	var move_to = input_handler._get_movement()
 	# movement is absolute!!
-	#print(movement)
 	set_translation(limit_paddle_range(Vector3(move_to.x, move_to.y, get_translation().z)))
-	#var rest = move(Vector3(movement.x, -movement.y, 0.0) * .1)
-	#if is_colliding():
-	#	#move(rest.slide(get_collision_normal()))
-	#	move(get_collision_normal().slide(rest))
-	
-	#movement = Vector2() # set to zero
-#	# TODO: mouse movement
-#	pass
--- game/scripts/InputHandler/MouseMovement.gd
+++ game/scripts/InputHandler/MouseMovement.gd
@@ -5,11 +5,13 @@ var move_to = Vector2()
 
 func _ready():
 	set_process_input(true)
-	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 func _input(event):
 	if (event.type == InputEvent.MOUSE_MOTION):
 		move_to = move_to + (event.relative_pos * Vector2(1.0, -1.0) * sensitivity)
+
 
 func _get_movement():
 	return move_to
