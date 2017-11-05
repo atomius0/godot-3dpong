6c155321f5ee2c19b54df8f633ecd8ad3de96322
atomius <atomius1@gmail.com>
2017-11-05 14:09:42 +0100

    tried to hide the mouse cursor when it is captured and discovered a bug in godot.
    the mouse cursor should be hidden when captured. but it stays visible instead.
    capturing and then hiding the mouse won't work, since these two modes are exclusive.
    added an idea for a workaround in the comments, TODO: implement it!
    
    (I also set the InputHandler script of Paddle1 back to mouse control.)

--- game/scripts/InputHandler/MouseMovement.gd
+++ game/scripts/InputHandler/MouseMovement.gd
@@ -1,11 +1,24 @@
 extends Node
 
+# TODO: disable mouse capture when this node gets deleted!
+
 var sensitivity = 0.15
 var movement = Vector2()
 
 func _ready():
 	set_process_input(true)
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	# there is a bug, that apparently makes it impossible to hide the mouse when it is captured...
+	# https://github.com/godotengine/godot/issues/5051
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED | Input.MOUSE_MODE_HIDDEN)
+	# these two overwrite each other:
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+	# so let's just hide the mouse and "capture" it via code (in _input):
+	# -> nope. capturing via code causes input events, which calls _input(), which causes an infinite loop...
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	# Workaround: capture the mouse and define a custom cursor, which is invisible.
+	#TODO: try this!
 
 
 func _input(event):
