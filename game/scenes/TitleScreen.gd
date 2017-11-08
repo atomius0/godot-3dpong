4ae3f2b5160eddbc3a870e92ebcfce822a96f15e
atomius <atomius1@gmail.com>
2017-11-08 20:56:26 +0100

    added skeleton code for menu input event handling.

--- game/scenes/TitleScreen.gd
+++ game/scenes/TitleScreen.gd
@@ -5,10 +5,10 @@ onready var option_count = options.get_child_count()
 onready var selector = get_node("Selector")
 
 func _ready():
-	print("option_count: %s" % [option_count])
+	#print("option_count: %s" % [option_count])
 	#selector.set_global_pos(Vector2(selector.get_global_pos().x, options.get_child(0).get_global_pos().y))
 	#selector.set_global_pos(Vector2(100,100))
-	print("pos: %s" % [options.get_child(0).get_global_pos()])
+	#print("pos: %s" % [options.get_child(0).get_global_pos()])
 	place_selector(0)
 	
 	set_process_input(true)
@@ -16,8 +16,16 @@ func _ready():
 
 
 func _input(event):
-	if (event.is_action_pressed("ui_page_up")):
-		print("ui_page_up")
+	if (event.is_action_pressed("menu_up")):
+		print("menu_up")
+	elif (event.is_action_pressed("menu_down")):
+		print("menu_down")
+	elif (event.is_action_pressed("menu_left")):
+		print("menu_left")
+	elif (event.is_action_pressed("menu_right")):
+		print("menu_right")
+	elif (event.is_action_pressed("menu_accept")):
+		print("menu_accept")
 	pass
 
 
