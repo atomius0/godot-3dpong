090c44d3b3170efe4176ccedda4f331ea0634966
atomius <atomius1@gmail.com>
2017-11-08 20:56:26 +0100

    more changes / tests in TitleScreen...

--- game/scenes/TitleScreen.gd
+++ game/scenes/TitleScreen.gd
@@ -6,7 +6,20 @@ onready var selector = get_node("Selector")
 
 func _ready():
 	print("option_count: %s" % [option_count])
-	selector.set_global_pos(Vector2(selector.get_global_pos().x, options.get_child(0).get_global_pos().y))
+	#selector.set_global_pos(Vector2(selector.get_global_pos().x, options.get_child(0).get_global_pos().y))
 	#selector.set_global_pos(Vector2(100,100))
 	print("pos: %s" % [options.get_child(0).get_global_pos()])
+	place_selector(0)
+	
+	set_process_input(true)
 	pass
+
+
+func _input(event):
+	if (event.is_action_pressed("ui_page_up")):
+		print("ui_page_up")
+	pass
+
+
+func place_selector(option_idx):
+	selector.set_global_pos(Vector2(selector.get_global_pos().x, options.get_child(option_idx).get_global_pos().y))
